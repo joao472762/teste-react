6247f4f5d0bc9bf914f675dffae803913317c420
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-01-16 16:31:11 -0300

    projeto criado

---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+<link rel="stylesheet" href="" />
+
+export function Home(){
+    return(
+        <div className="content">
+            <aside>
+                <h1>
+                    Nunca mais perca a hora de Tomar os Remédios
+                </h1>
+                <img src="GoogleIcon.svg" alt="kddkddkd" />
+                
+            </aside>
+        </div>
+    );
+}
